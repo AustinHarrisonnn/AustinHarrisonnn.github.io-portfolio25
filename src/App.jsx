fcacb4149cb2408b7359ce728c56c32029f7d6ce
Austin Harrison <austin.harrisonn@yahoo.com>
2025-02-13 17:34:17 -0500

    added inital containers and css styling.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,28 @@ import './App.css'
 
 function App() {
   return (
-    <h1>Austin Harrison</h1>
+    <div className="App">
+      <header className="header">
+        <h2>Austin Harrison ✿</h2>
+        <nav className="nav-links">
+          <a href="https://github.com/AustinHarrisonnn">Github</a>
+          <a href="https://github.com/AustinHarrisonnn">LinkedIn</a>
+        </nav>
+      </header>
+      <div className="photo-body">
+        <div className="intro">
+          <h1>Senior student at Florida Atlantic University interested in software engineering & web development</h1>
+        </div>
+        <div className="image-frame">
+          <img src="src\assets\profile.JPG" alt="Me in spain!" />
+        </div>
+      </div>
+      <div className="project-container">
+        <div className="project-header">
+          <h2>Projects</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 

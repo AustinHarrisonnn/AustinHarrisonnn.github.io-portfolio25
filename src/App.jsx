df5a77bc09f7101d1bdc553aa11deb2dfec53bf2
Austin Harrison <austin.harrisonn@yahoo.com>
2025-02-14 14:14:45 -0500

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,28 @@ import './App.css'
 function App() {
   return (
     <div className="App">
+      <section class="spikes">
       <header className="header">
-        <h2>Austin Harrison ✿</h2>
+        <h2>
+          <span className="color1">A</span>
+          <span className="color2">u</span>
+          <span className="color3">s</span>
+          <span className="color4">t</span>
+          <span className="color5">i</span>
+          <span className="color6">n</span>
+          <span className="color7"> </span>
+          <span className="color8">H</span>
+          <span className="color9">a</span>
+          <span className="color10">r</span>
+          <span className="color11">r</span>
+          <span className="color12">i</span>
+          <span className="color13">s</span>
+          <span className="color14">o</span>
+          <span className="color15">n</span>
+        </h2>
         <nav className="nav-links">
-          <a href="https://github.com/AustinHarrisonnn">Github</a>
-          <a href="https://github.com/AustinHarrisonnn">LinkedIn</a>
+          <a href="https://github.com/AustinHarrisonnn"><img id='github' src='src\assets\icons8-github-85.png'/></a>
+          <a href="https://github.com/AustinHarrisonnn"><img id='linkedin' src='src\assets\icons8-linkedin-85.png'/></a>
         </nav>
       </header>
       <div className="photo-body">
@@ -19,6 +36,8 @@ function App() {
           <img src="src\assets\profile.JPG" alt="Me in spain!" />
         </div>
       </div>
+      <br></br>
+      </section>
       <div className="project-container">
         <div className="project-header">
           <h2>Projects</h2>
